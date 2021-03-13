d3cf4087ad584273925b24ba53469bbc9601208d
Tiago Prata <prataaa@hotmail.com>
2021-03-13 02:02:55 -0300

    Shadow crop working in all directions

--- KeatingShot/KeatingShot/Shadow.cs
+++ KeatingShot/KeatingShot/Shadow.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -6,6 +7,9 @@ namespace KeatingShot
 {
     public partial class frmShadow : Form
     {
+        private int mouseOnClickPositionX;
+        private int mouseOnClickPositionY;
+
         public frmShadow()
         {
             InitializeComponent();
@@ -25,6 +29,9 @@ namespace KeatingShot
 
         private void frmShadow_MouseDown(object sender, MouseEventArgs e)
         {
+            mouseOnClickPositionX = e.X;
+            mouseOnClickPositionY = e.Y;
+
             this.focusArea1.Location = new Point(e.X, e.Y);
             this.focusArea1.Size = new Size(1, 1);
             this.focusArea1.Visible = true;
@@ -36,14 +43,55 @@ namespace KeatingShot
         {
             if (e.Button == MouseButtons.Left)
             {
-                focusArea1.Width = e.X - focusArea1.Location.X;
-                focusArea1.Height = e.Y - focusArea1.Location.Y;
+                if (GetMouseXDirection(e) == ArrowDirection.Right && GetMouseYDirection(e) == ArrowDirection.Down)
+                {
+                    focusArea1.Width = e.X - focusArea1.Location.X;
+                    focusArea1.Height = e.Y - focusArea1.Location.Y;
+                }
+                if (GetMouseXDirection(e) == ArrowDirection.Left && GetMouseYDirection(e) == ArrowDirection.Down)
+                {
+                    focusArea1.Location = new Point(e.X, focusArea1.Location.Y);
+                    focusArea1.Width = mouseOnClickPositionX - focusArea1.Location.X;
+                    focusArea1.Height = e.Y - focusArea1.Location.Y;
+                }
+                if (GetMouseXDirection(e) == ArrowDirection.Left && GetMouseYDirection(e) == ArrowDirection.Up)
+                {
+                    focusArea1.Location = new Point(e.X, e.Y);
+                    focusArea1.Width = mouseOnClickPositionX - focusArea1.Location.X;
+                    focusArea1.Height = mouseOnClickPositionY - focusArea1.Location.Y;
+                }
+                if (GetMouseXDirection(e) == ArrowDirection.Right && GetMouseYDirection(e) == ArrowDirection.Up)
+                {
+                    focusArea1.Location = new Point(focusArea1.Location.X, e.Y);
+                    focusArea1.Width = e.X - focusArea1.Location.X;
+                    focusArea1.Height = mouseOnClickPositionY - focusArea1.Location.Y;
+                }
             }
         }
 
         public void HideFocusAreas()
         {
             focusArea1.Visible = false;
+        }
+
+        public ArrowDirection GetMouseXDirection(MouseEventArgs e)
+        {
+            ArrowDirection direction;
+
+            double deltaDirection = mouseOnClickPositionX - e.X;
+            direction = deltaDirection > 0 ? ArrowDirection.Left : ArrowDirection.Right;
+            
+            return direction;
+        }
+
+        public ArrowDirection GetMouseYDirection(MouseEventArgs e)
+        {
+            ArrowDirection direction;
+
+            double deltaDirection = mouseOnClickPositionY - e.Y;
+            direction = deltaDirection > 0 ? ArrowDirection.Up : ArrowDirection.Down;
+            
+            return direction;
         }
     }
 }
